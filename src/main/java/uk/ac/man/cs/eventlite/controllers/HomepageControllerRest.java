75cce8aad59cbbb0aa95c32d55807db7f84db83a
Robert Haines <rhaines@manchester.ac.uk>
2017-04-23 22:13:04 +0100

    Pass the homepage test by providing a JSON response.

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageControllerRest.java
@@ -0,0 +1,17 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomepageControllerRest {
+
+	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public String getRoot() {
+
+		return "home/index";
+	}
+}
