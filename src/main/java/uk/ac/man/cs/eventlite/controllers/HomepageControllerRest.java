c4b23ce5afddbbb38b993d8c0b4657b2a528a5fb
Robert Haines <rhaines@manchester.ac.uk>
2017-04-25 21:53:17 +0100

    Implement programatically built _self links.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageControllerRest.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomepageControllerRest.java
@@ -2,15 +2,21 @@ package uk.ac.man.cs.eventlite.controllers;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @RequestMapping("/")
 public class HomepageControllerRest {
 
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public String getRoot() {
+	public String getRoot(Model model, UriComponentsBuilder b) {
+
+		UriComponents link = b.path("/").build();
+		model.addAttribute("self_link", link.toUri());
 
 		return "home/index";
 	}
